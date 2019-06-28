eff0723cf51fa4ce004f0800559538c877ed657d
reslie <1550167691@qq.com>
2019-06-28 16:22:57 +0800

    update thirde third page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,7 +9,9 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+
     <Link to="/page-3/">Go to ThirdPage</Link>
+    <br/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
